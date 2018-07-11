f22f8dc6470a1890147dbc1206d85c084df4d0e2
huynq <qhuydtvt@gmail.com>
2018-07-11 12:54:14 +0700

    Add remove unncessary cod

--- niche.py
+++ niche.py
@@ -64,61 +64,61 @@ for option in options:
 
 # pyexcel.save_as(records=school_list, dest_file_name="best-colleges-for-finance-accounting.xlsx")
 
-html_options = '''
-<div>
-  <option value="best-colleges">All</option>
-  <option selected value="best-colleges-for-finance-accounting">Accounting/Finance</option>
-  <option value="best-colleges-for-administrative-and-clerical">Administrative Work</option>
-  <option value="best-colleges-for-agricultural-sciences">Agricultural Sciences</option>
-  <option value="best-colleges-for-alternative-medicine">Alternative Medicine</option>
-  <option value="best-colleges-for-anthropology-archaeology">Anthropology</option>
-  <option value="best-colleges-for-architecture">Architecture</option>
-  <option value="best-colleges-for-visual-arts">Art</option>
-  <option value="best-colleges-for-arts-management">Arts Management</option>
-  <option value="best-colleges-for-biology">Biology</option>
-  <option value="best-colleges-for-building-and-construction">Building and Construction</option>
-  <option value="best-colleges-for-business-management">Business</option>
-  <option value="best-colleges-for-chemistry">Chemistry</option>
-  <option value="best-colleges-for-communications-journalism">Communications</option>
-  <option value="best-colleges-for-computer-science">Computer Science</option>
-  <option value="best-colleges-for-cosmetology">Cosmetology</option>
-  <option value="best-colleges-for-criminal-justice">Criminal Justice</option>
-  <option value="best-colleges-for-culinary-arts-and-food-service">Culinary Arts</option>
-  <option value="best-colleges-for-dental">Dental Studies</option>
-  <option value="best-colleges-for-design">Design</option>
-  <option value="best-colleges-for-economics">Economics</option>
-  <option value="best-colleges-for-education">Education</option>
-  <option value="best-colleges-for-engineering">Engineering</option>
-  <option value="best-colleges-for-english">English</option>
-  <option value="best-colleges-for-earth-and-environmental-sciences">Environmental Science</option>
-  <option value="best-colleges-for-film-video-and-photography">Film/Photography</option>
-  <option value="best-colleges-for-food-and-nutrition">Food and Nutrition</option>
-  <option value="best-colleges-for-foreign-languages">Foreign Languages</option>
-  <option value="best-colleges-for-general-studies">General Studies</option>
-  <option value="best-colleges-for-health-professions">Health Professions</option>
-  <option value="best-colleges-for-history">History</option>
-  <option value="best-colleges-for-humanities">Humanities/Social Sciences</option>
-  <option value="best-colleges-for-information-technology">Information Technology</option>
-  <option value="best-colleges-for-international-relations">International Relations</option>
-  <option value="best-colleges-for-rehabilitation-and-therapy">Kinesiology and Therapy</option>
-  <option value="best-colleges-for-legal-studies">Legal Studies</option>
-  <option value="best-colleges-for-mathematics">Math</option>
-  <option value="best-colleges-for-mechanics-repair">Mechanics</option>
-  <option value="best-colleges-for-medical-assistant-technician">Medical Technicians</option>
-  <option value="best-colleges-for-music">Music</option>
-  <option value="best-colleges-for-nursing">Nursing</option>
-  <option value="best-colleges-for-performing-arts">Performing Arts/Theater</option>
-  <option value="best-colleges-for-pharmacy">Pharmacy</option>
-  <option value="best-colleges-for-philosophy">Philosophy</option>
-  <option value="best-colleges-for-physics">Physics</option>
-  <option value="best-colleges-for-political-science">Political Science</option>
-  <option value="best-colleges-for-protective-services">Protective Services</option>
-  <option value="best-colleges-for-psychology">Psychology</option>
-  <option value="best-colleges-for-religious-studies-theology">Religious Studies</option>
-  <option value="best-colleges-for-trades-and-personal-services">Trades and Personal Services</option>
-  <option value="best-colleges-for-veterinary-studies">Veterinary Studies</option>
-</div>
-'''
+# html_options = '''
+# <div>
+#   <option value="best-colleges">All</option>
+#   <option selected value="best-colleges-for-finance-accounting">Accounting/Finance</option>
+#   <option value="best-colleges-for-administrative-and-clerical">Administrative Work</option>
+#   <option value="best-colleges-for-agricultural-sciences">Agricultural Sciences</option>
+#   <option value="best-colleges-for-alternative-medicine">Alternative Medicine</option>
+#   <option value="best-colleges-for-anthropology-archaeology">Anthropology</option>
+#   <option value="best-colleges-for-architecture">Architecture</option>
+#   <option value="best-colleges-for-visual-arts">Art</option>
+#   <option value="best-colleges-for-arts-management">Arts Management</option>
+#   <option value="best-colleges-for-biology">Biology</option>
+#   <option value="best-colleges-for-building-and-construction">Building and Construction</option>
+#   <option value="best-colleges-for-business-management">Business</option>
+#   <option value="best-colleges-for-chemistry">Chemistry</option>
+#   <option value="best-colleges-for-communications-journalism">Communications</option>
+#   <option value="best-colleges-for-computer-science">Computer Science</option>
+#   <option value="best-colleges-for-cosmetology">Cosmetology</option>
+#   <option value="best-colleges-for-criminal-justice">Criminal Justice</option>
+#   <option value="best-colleges-for-culinary-arts-and-food-service">Culinary Arts</option>
+#   <option value="best-colleges-for-dental">Dental Studies</option>
+#   <option value="best-colleges-for-design">Design</option>
+#   <option value="best-colleges-for-economics">Economics</option>
+#   <option value="best-colleges-for-education">Education</option>
+#   <option value="best-colleges-for-engineering">Engineering</option>
+#   <option value="best-colleges-for-english">English</option>
+#   <option value="best-colleges-for-earth-and-environmental-sciences">Environmental Science</option>
+#   <option value="best-colleges-for-film-video-and-photography">Film/Photography</option>
+#   <option value="best-colleges-for-food-and-nutrition">Food and Nutrition</option>
+#   <option value="best-colleges-for-foreign-languages">Foreign Languages</option>
+#   <option value="best-colleges-for-general-studies">General Studies</option>
+#   <option value="best-colleges-for-health-professions">Health Professions</option>
+#   <option value="best-colleges-for-history">History</option>
+#   <option value="best-colleges-for-humanities">Humanities/Social Sciences</option>
+#   <option value="best-colleges-for-information-technology">Information Technology</option>
+#   <option value="best-colleges-for-international-relations">International Relations</option>
+#   <option value="best-colleges-for-rehabilitation-and-therapy">Kinesiology and Therapy</option>
+#   <option value="best-colleges-for-legal-studies">Legal Studies</option>
+#   <option value="best-colleges-for-mathematics">Math</option>
+#   <option value="best-colleges-for-mechanics-repair">Mechanics</option>
+#   <option value="best-colleges-for-medical-assistant-technician">Medical Technicians</option>
+#   <option value="best-colleges-for-music">Music</option>
+#   <option value="best-colleges-for-nursing">Nursing</option>
+#   <option value="best-colleges-for-performing-arts">Performing Arts/Theater</option>
+#   <option value="best-colleges-for-pharmacy">Pharmacy</option>
+#   <option value="best-colleges-for-philosophy">Philosophy</option>
+#   <option value="best-colleges-for-physics">Physics</option>
+#   <option value="best-colleges-for-political-science">Political Science</option>
+#   <option value="best-colleges-for-protective-services">Protective Services</option>
+#   <option value="best-colleges-for-psychology">Psychology</option>
+#   <option value="best-colleges-for-religious-studies-theology">Religious Studies</option>
+#   <option value="best-colleges-for-trades-and-personal-services">Trades and Personal Services</option>
+#   <option value="best-colleges-for-veterinary-studies">Veterinary Studies</option>
+# </div>
+# '''
 
 # from bs4 import BeautifulSoup
 # import json
